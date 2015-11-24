ab20727cb4cdf7878381c1f6210de1fdf39362eb
itslikeme <andrealbinomarques@hotmail.com>
2015-11-24 14:37:51 -0200

    Funcionalidade de consulta de metereologia adicionada.

--- kass.py
+++ kass.py
@@ -79,6 +79,56 @@ class Kass:
 				print str(element_index) + '. ' + i + '\n'
 				element_index+=1
 
+
+	@staticmethod
+	def climate(city):
+		import pyowm
+		API_KEY = '88bef2e2314e0affe5c32f6caf8a3a4d'
+		owm = pyowm.OWM(API_KEY)
+
+		observation = owm.weather_at_place(city)
+		w = observation.get_weather()
+		l = observation.get_location()
+
+		class Forecast():
+			def __init__(self, city_name, city_temp, city_status,city_pressure, city_sunrise,city_humidity,city_wind,city_time,city_lon,city_lat,city_ID,city_rain,city_sunset):
+				self.name = city_name
+				self.status = city_status
+				self.pressure = city_pressure
+				self.sunrise = city_sunrise
+				self.humidity = city_humidity
+				self.wind = city_wind
+				self.time = city_time
+				self.lon = city_lon
+				self.lat = city_lat
+				self.ID = city_ID
+				self.rain = city_rain
+				self.sunset = city_sunset
+
+				def convertTemp(city_temp):
+					for i in city_temp:
+						if(i == 'temp'):
+							self.temp = city_temp[i]
+				convertTemp(city_temp)
+
+				def display_info():
+					print '\n____________________________________________\n'
+					print 'Hora da consulta:  ' + str(self.time) + '\n'
+					print 'Informacao metereologica da cidade "' + str(self.name) + '" :\n'
+					print 'Nome: ' + str(self.name) + ' - ID: ' + str(self.ID)
+					print 'Geolocation: Lat(' + str(self.lat) + ') Lon(' + str(self.lon) + ')' 
+					print 'Temperatura: ' + str(self.temp) + ' Celsius'
+					print 'Chuva: ' + str(self.rain)
+					print 'Pressao Atmosferica: ' + str(self.pressure)
+					print 'Humidade relativa do ar: ' + str(self.humidity)
+					print 'Nascer do Sol: ' + str(self.sunrise)
+					print 'Por do Sol: ' + str(self.sunset)
+					print '\n____________________________________________\n'
+				display_info()
+				
+
+		weatherForecast = Forecast(l.get_name(),w.get_temperature(unit='celsius'),w.get_detailed_status(),w.get_pressure(),w.get_sunrise_time('iso'),w.get_humidity(), w.get_wind(),w.get_reference_time(timeformat='iso'),l.get_lon(),l.get_lat(),l.get_ID(),w.get_rain(),w.get_sunset_time('iso'))
+
 class Database:
 	db_name = 'Kass.db'
 	db_Path = Program.script.current_folder + '/' + db_name
@@ -452,6 +502,32 @@ class Interpreter:
 					Kass.print_list(Database.list(),str(string_analysis[0]).upper())
 				else:
 					Kass.print_list(Database.list(),None)
+
+		previsao = ['previsao','temperatura','metereologia','clima']
+		for i in previsao:
+			if(string[0:len(i)] == str(i)):
+				UND = True
+				string_analysis = string.split(' ')
+				if(i in string_analysis):
+					string_analysis.remove(i)
+				if(len(string_analysis) > 0):
+					confString = 'Posso buscar o relatorio climatologico da cidade de ' + str(string_analysis[0]) + '?'
+					Kass.talk(confString)
+					confirmation = raw_input('\n Operador: ')
+					posOptions = ['sim','ok','mostrar','visualizar','emitir','efetuar','tudo bem','em frente','vamos la','vamos lá']
+					if(confirmation in posOptions):
+						Kass.climate(string_analysis[0])
+					else:
+						talkOptions_00 = ['Pesquisa cancelada','Ok, cancelar então.','Você que sabe.', "Tudo bem. Abortando...",'Tudo bem.','Pronto.']
+						Kass.random_answer(talkOptions_00)
+				else:
+					explainString = 'O formato para consultar metereologia é "CIDADE,PAIS" como por exemplo "ASSIS,BR'
+					talkString = 'De qual cidade?'
+					Kass.talk(talkString)
+					city = raw_input('\n Operador: ')
+					Kass.climate(city)
+
+
 		else:
 			if UND == False:
 				result, tString = Interpreter.analyze(string)
--- weather.py
+++ weather.py
@@ -1,7 +1,48 @@
-import urllib2
+#coding: UTF-8
 
-var = urllib2.urlopen("http://www.weather.com/weather/today/l/-22.66,-50.42?lat=-22.66&lon=-50.42&locale=pt_BR&temp=com")
-html = var.read()
-print html
-inder = str(html).find('device')
-#print str(inder)
+import pyowm
+API_KEY = '88bef2e2314e0affe5c32f6caf8a3a4d'
+owm = pyowm.OWM(API_KEY)
+
+observation = owm.weather_at_place('ASSIS,BR')
+w = observation.get_weather()
+l = observation.get_location()
+
+class Forecast():
+	def __init__(self, city_name, city_temp, city_status,city_pressure, city_sunrise,city_humidity,city_wind,city_time,city_lon,city_lat,city_ID,city_rain,city_sunset):
+		self.name = city_name
+		self.status = city_status
+		self.pressure = city_pressure
+		self.sunrise = city_sunrise
+		self.humidity = city_humidity
+		self.wind = city_wind
+		self.time = city_time
+		self.lon = city_lon
+		self.lat = city_lat
+		self.ID = city_ID
+		self.rain = city_rain
+		self.sunset = city_sunset
+
+		def convertTemp(city_temp):
+			for i in city_temp:
+				if(i == 'temp'):
+					self.temp = city_temp[i]
+		convertTemp(city_temp)
+
+		def display_info():
+			print '\n____________________________________________\n'
+			print 'Hora da consulta:  ' + str(self.time) + '\n'
+			print 'Informacao metereologica da cidade "' + str(self.name) + '" :\n'
+			print 'Nome: ' + str(self.name) + ' - ID: ' + str(self.ID)
+			print 'Geolocation: Lat(' + str(self.lat) + ') Lon(' + str(self.lon) + ')' 
+			print 'Temperatura: ' + str(self.temp) + ' Celsius'
+			print 'Chuva: ' + str(self.rain)
+			print 'Pressao Atmosferica: ' + str(self.pressure)
+			print 'Humidade relativa do ar: ' + str(self.humidity)
+			print 'Nascer do Sol: ' + str(self.sunrise)
+			print 'Por do Sol: ' + str(self.sunset)
+			print '\n____________________________________________\n'
+		display_info()
+		
+
+weatherForecast = Forecast(l.get_name(),w.get_temperature(unit='celsius'),w.get_detailed_status(),w.get_pressure(),w.get_sunrise_time('iso'),w.get_humidity(), w.get_wind(),w.get_reference_time(timeformat='iso'),l.get_lon(),l.get_lat(),l.get_ID(),w.get_rain(),w.get_sunset_time('iso'))
